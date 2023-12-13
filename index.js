172bde3fed70031f2011a249c485da660356ff80
miriam <miriamwangui525@gmail.com>
2023-12-13 20:56:44 +0300

    control lab

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+ if (distance <= 400) {
+    return 'This one is on me!';
+ } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+ } else if (distance > 2500) {
+    return 'No can do.';
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+console.log(scuberGreetingForFeet(2500));
+
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
+
+
+const city1 = 'NYC';
+const city2 = 'Pittsburgh';
+
+console.log(ternaryCheckCity(city1));
+console.log(ternaryCheckCity(city2)); 
+
 
 function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }};
+
+  const tip = switchOnCharmFromTip('generous');
+  console.log(tip); 
+  
+  const tip1 = switchOnCharmFromTip('generous');  
+  const tip2 = switchOnCharmFromTip('not as generous');
+  
+  console.log(tip1); // Output: Thank you so much.
+  console.log(tip2); // Output: Thank you.
--- test/indexTest.js
+++ test/indexTest.js
@@ -23,17 +23,22 @@ describe('index.js', function () {
     });
   });
 
-  describe('switchOnCharmFromTip()', function () {
-    it('should return "Thank you so much." if the tip is generous', function () {
-      expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
-    });
-
-    it('should return "Thank you." if the tip is not as generous', function () {
-      expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
-    });
-
-    it('should return "Bye." if anything else', function () {
-      expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
-    });
-  });
-});
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
+  
+  const tip1 = 'generous';
+  const tip2 = 'not as generous';
+  const tip3 = 'thanks for everything';
+  
+  console.log(switchOnCharmFromTip(tip1)); 
+  console.log(switchOnCharmFromTip(tip2));
+  console.log(switchOnCharmFromTip(tip3)); 
+})
